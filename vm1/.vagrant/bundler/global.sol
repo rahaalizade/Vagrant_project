8a1d272fbd44660db8be92fd2695c72f4535f6db
fatemealizade <alizade.fateme78@gmail.com>
2022-09-18 14:34:03 +0430

    Initial commit

---
+++ vm1/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-cachier",["= 1.2.1"]],["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"759c7c22894aa9a2fe737155ae6a6e6303c232132f1f7f7046273f77ceda1bc4","vagrant_version":"2.2.19"}
